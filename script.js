f4a2c84e90757c56c7e1484f316f358093fe6521
Gaspatt <matheusgaspar29.if@gmail.com>
2023-11-29 09:25:57 -0300

    n

---
+++ script.js
@@ -0,0 +1,47 @@
+const tarefaInput = document.getElementById("tarefa");
+const btnAdc = document.getElementById("adicionar");
+const listaTarefas = document.getElementById("tarefas");
+const listaFinalizadas = document.getElementById("finalizadas")
+
+
+
+btnAdc.addEventListener("click", adicionarTarefa);
+tarefaInput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        adicionarTarefa();
+    }
+});
+
+
+
+function adicionarTarefa() {
+    const tarefaTexto = tarefaInput.value;
+    if (tarefaTexto.trim() !== "") {
+        const novaTarefa = document.createElement("li");
+        novaTarefa.innerHTML = `
+          ${tarefaTexto} <button class="excluir">Excluir</button> <button class="finalizar">Finalizar</button>
+      `;
+        listaTarefas.appendChild(novaTarefa);
+        tarefaInput.value = "";
+    }
+}
+
+
+listaTarefas.addEventListener("click", function (e) {
+    if (e.target.classList.contains("excluir")) {
+        e.target.parentElement.remove();
+    }
+    if (e.target.classList.contains("finalizar")) {
+        listaFinalizadas.appendChild(e.target.parentElement)
+    }
+});
+listaFinalizadas.addEventListener("click", function (e) {
+    if (e.target.classList.contains("excluir")) {
+        e.target.parentElement.remove()
+    }
+
+})
+
+
+
+
